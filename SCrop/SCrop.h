d4d2571d512c72a10d5a9a73d072c05803701a8b
Puasonych <basargin.erik@gmail.com>
2019-04-04 11:57:31 +0500

    Clean project

---
+++ SCrop/SCrop.h
@@ -0,0 +1,19 @@
+//
+//  SCrop.h
+//  SCrop
+//
+//  Created by Eric Basargin on 04/04/2019.
+//  Copyright © 2019 Three man army. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCrop.
+FOUNDATION_EXPORT double SCropVersionNumber;
+
+//! Project version string for SCrop.
+FOUNDATION_EXPORT const unsigned char SCropVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCrop/PublicHeader.h>
+
+
